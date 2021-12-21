2e1f6426280624d77af87ca725f257980a8d1bca
teirm <tribunus.vii@gmail.com>
2021-12-21 16:46:18 -0500

    Basic simulation of Chat UI is complete with user list

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,20 @@
 #include <memory>
 #include <vector>
 #include <deque>
+#include <list>
 #include <ftxui/dom/elements.hpp>  
 #include <ftxui/screen/screen.hpp>
 #include <string>
 #include <thread>
 #include <mutex>
+#include <cstdlib>
 
-#include "ftxui/component/captured_mouse.hpp"  // for ftxui
-#include "ftxui/component/component.hpp"  // for Checkbox, Renderer, Horizontal, Vertical, Menu, Radiobox, Tab, Toggle
-#include "ftxui/component/component_base.hpp"     // for ComponentBase
-#include "ftxui/component/component_options.hpp"  // for InputOption
-#include "ftxui/component/event.hpp"              // for Event, Event::Custom
-#include "ftxui/component/screen_interactive.hpp"  // for Component, ScreenInteractive
+#include "ftxui/component/captured_mouse.hpp"       // for ftxui
+#include "ftxui/component/component.hpp"            // for Checkbox, Renderer, Horizontal, Vertical, Menu, Radiobox, Tab, Toggle
+#include "ftxui/component/component_base.hpp"       // for ComponentBase
+#include "ftxui/component/component_options.hpp"    // for InputOption
+#include "ftxui/component/event.hpp"                // for Event, Event::Custom
+#include "ftxui/component/screen_interactive.hpp"   // for Component, ScreenInteractive
 #include "ftxui/screen/color.hpp"  
 #include "ftxui/screen/box.hpp"  
 #include "ftxui/dom/node.hpp"
@@ -53,6 +55,12 @@ int main(int argc, const char *argv[]) {
                 text(u.input),
                 });
     };
+    
+    std::mutex users_lock;
+    std::list<Element> current_users = {
+        hbox({text("User 1") | color(Color::Green)}),
+        hbox({text("User 2") | color(Color::Yellow)}),
+    };
 
     auto main_renderer = Renderer(input, [&] {
         auto input_win = window(text("Input: "), 
@@ -60,21 +68,25 @@ int main(int argc, const char *argv[]) {
 
         
         Elements elements;
+        deque_lock.lock();
         for (auto &entry : entries) {
             elements.push_back(renderInput(entry));
         }
+        deque_lock.unlock(); 
 
-        auto dimensions = Terminal::Size(); 
+        Elements users;
+        users_lock.lock();
+        for (auto &user : current_users) {
+            users.push_back(user);
+        }
+        users_lock.unlock();
 
         return vbox({
                 hbox({
                     vbox(
                         std::move(elements)
                     ) | frame | size(WIDTH, GREATER_THAN, 200) |  size(HEIGHT, EQUAL, 20) | border, 
-                    vbox({
-                            text("User 1"),
-                            text("User 2"),
-                    }) | frame | size(WIDTH, EQUAL, 60) | border 
+                    vbox(std::move(users)) | frame | size(WIDTH, EQUAL, 60) | border 
                 }), 
                 input_win});
     });
@@ -101,11 +113,39 @@ int main(int argc, const char *argv[]) {
             std::this_thread::sleep_for(1s);
         }
     });
+    
+    bool manipulate_users = true;
+    std::thread user_manip([&] {
+        std::srand(std::time(nullptr));
+        while (manipulate_users) {
+            auto action = std::rand() % 3;
+            users_lock.lock();
+            switch (action) {
+            case 1:
+                /* remove user */
+                if (current_users.size() > 0) {
+                    current_users.pop_front();
+                }
+                break;
+            default:
+                /* Add a user */
+                current_users.push_back(hbox({text("User " + std::to_string(action+1)) | color(Color::Purple)}));
+                break;
+            }
+            users_lock.unlock();
+            screen.PostEvent(Event::Custom);
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(3s);
+        }
+    });
 
     screen.Loop(main_renderer);
     
     run_maru = false;
     maru.join();
+    
+    manipulate_users = false;
+    user_manip.join();
 
     return 0;
 }
